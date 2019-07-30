9a59a32265057399b2e904d52f9fa2bf13fe1171
wgxo <werner.garcia@crossover.com>
2019-07-30 00:36:00 +0000

    Fixed deploy scripts

--- build.sh
+++ build.sh
@@ -7,11 +7,13 @@ LIGHT_GRAY="\033[0;37m"; BLUE="\033[1;36m"; RED="\033[0;31m"; LIGHT_RED="\033[1;
 GREEN="\033[1;32m"; WHITE="\033[1;37m"; LIGHT_GRAY="\033[0;37m"; YELLOW="\033[1;33m";
 NOCOLOR="\033[0m"
 
-MYSQL_SERVER="mysql"
-MYSQL_PASS="OGYxYmI1OTUzZmM"
+MYSQL_SERVER="aurora5.aureacentral.com"
+MYSQL_DATABASE="clone_p_kyk_kc03_db_5829589"
+MYSQL_USER="Werner Garcia"
+MYSQL_PASS="Welcome123!"
 
-# PHPStorm development machine MAC address
-MAC="00:0c:29:58:25:aa"
+# PHPStorm development machine MAC address (comment out to disable)
+#MAC="00:0c:29:58:25:aa"
 
 LINES=`tput lines`
 COLS=`tput cols`
@@ -24,8 +26,13 @@ echo "USER=$USER" >> .env
 ##### MySQL Stuff #####
 perl -pi -e "s/(?=('DB_HOSTNAME', ))'.*'/\\1'$MYSQL_SERVER'/" \
         ./swift/kayako-SWIFT/trunk/__swift/config/config.php
+perl -pi -e "s/(?=('DB_NAME', ))'.*'/\\1'$MYSQL_DATABASE'/" \
+        ./swift/kayako-SWIFT/trunk/__swift/config/config.php
+perl -pi -e "s/(?=('DB_USERNAME', ))'.*'/\\1'$MYSQL_USER'/" \
+        ./swift/kayako-SWIFT/trunk/__swift/config/config.php
 perl -pi -e "s/(?=('DB_PASSWORD', ))'.*'/\\1'$MYSQL_PASS'/" \
         ./swift/kayako-SWIFT/trunk/__swift/config/config.php
+
 
 # Enable debugging and ChromePHP logging
 perl -pi -e "s/(?=('SWIFT_DEBUG',))[^\)]*/\\1 true/" \
@@ -52,15 +59,12 @@ echo "GWIP=$GWIP" >> .env
 # I need the IP so KC inside the Docker container can establish a connection.
 # Since the network uses DHCP to get IPs, I only know the MAC address and I use the following
 # code to get the dynamic IP
-HOSTIP=$(arp -na |grep $MAC|grep -oP '\d+(\.\d+){3}'|head -1)
-
-if [ -z "$HOSTIP" ]; then
-		echo "Unable to get host IP address"
-		exit 1
+if [ ! -z "$MAC" ]; then
+    HOSTIP=$(arp -na |grep $MAC|grep -oP '\d+(\.\d+){3}'|head -1)
+    if [ ! -z "$HOSTIP" ]; then
+	echo "XDEBUG_CONFIG=idekey=PHPSTORM remote_host=$HOSTIP remote_enable=1 remote_autostart=1" >> .env
+    fi
 fi
-
-echo "XDEBUG_CONFIG=idekey=PHPSTORM remote_host=$HOSTIP remote_enable=1 remote_autostart=1" >> .env
-
 
 ##### Build it! #####
 if [ $# -gt 0 ]; then
@@ -78,4 +82,3 @@ echo "$GREEN*** Remember to check your database settings in $DIR/trunk/__swift/c
 grep "DB_" $DIR/trunk/__swift/config/config.php|head -4
 grep "^define.*DEBUG" $DIR/trunk/__swift/config/config.php
 grep "^define.*ENVIRON" $DIR/trunk/__swift/config/config.php
-
--- deploy-and-setup.sh
+++ deploy-and-setup.sh
@@ -3,13 +3,18 @@
 WORKSPACE="./swift/kayako-SWIFT"
 
 #####################################################
-### REPLACE YOUR EMAIL ADDRESS AND WEBHOST BELOW: ###
+### REPLACE YOUR EMAIL ADDRESS, WEBHOST AND GITHUB SSH KEY BELOW: ###
 #####################################################
 EMAIL="werner.garcia+kcadmin@crossover.com"
 # This is the host of the URL to access Kayako from a web browser: http://faster.xo.local/
-WEBHOST="faster.xo.local"
+WEBHOST="ip-10-66-20-35.ec2.internal"
+SSHKEY="~/.ssh/wgxo"
 
-[ -d ${WORKSPACE}/trunk ] || git clone git@github.com:trilogy-group/kayako-SWIFT ${WORKSPACE}
+[ -f `readlink -f ${SSHKEY}` ] || { echo "Github SSH key '${SSHKEY}' not found!" ; exit 1; }
+
+[ -d ${WORKSPACE}/trunk ] || ssh-agent bash -c "ssh-add ${SSHKEY}; git clone git@github.com:trilogy-group/kayako-SWIFT.git ${WORKSPACE}"
+
+[ -d ${WORKSPACE}/trunk ] || { echo "Unable to clone repository!" ; exit 1; }
 
 sh build.sh 1
 
@@ -27,4 +32,6 @@ WEBHOST=`echo ${WEBHOST} | perl -lpe 's/([^A-Za-z0-9])/sprintf("%%%02X", ord($1)
 
 [ $# -gt 0 -a "$1" = "-f" ] && mv ${WORKSPACE}/trunk/setup.bak ${WORKSPACE}/trunk/setup >/dev/null 2>&1
 
-[ -d ${WORKSPACE}/trunk/setup.bak ] || docker-compose exec swift bash -c "mysqladmin drop -f swift; mysqladmin create swift; su - wgarcia -c \"umask 002; cd setup;XDEBUG_CONFIG=0 php console.setup.php Trilogy http%3A%2F%2F${WEBHOST}%2F Kayako Admin admin admin ${EMAIL}; mv ~/setup ~/setup.bak\""
+DBNAME=$(grep -Poe '(?<=MYSQL_DATABASE=").+(?=")' build.sh)
+
+[ -d ${WORKSPACE}/trunk/setup.bak ] || docker-compose exec swift bash -c "mysqladmin drop -f ${DBNAME}; mysqladmin create ${DBNAME}; su - ${USER} -c \"umask 002; cd setup;XDEBUG_CONFIG=0 php console.setup.php Trilogy http%3A%2F%2F${WEBHOST}%2F Kayako Admin admin admin ${EMAIL}; mv ~/setup ~/setup.bak\""
